f49dadfb4fe21d5fcf78ff9d96fdbc61f0fce9fb
matisantacruzz <matias95ssc@gmail.com>
2023-02-02 00:34:33 -0300

    actualizacion src y link de bstrap

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,17 +1,28 @@
+import CartWidget from "../CartWidget";
 import React from "react";
 
 export const NavBar = () => {
     return ( 
-        <nav class="menu-navigation">
-      <a href="#inicio">Inicio</a>
-      <a href="#productos">Productos</a>
-      <a href="#contacto">Contacto</a>
-      <h2 class="mostrar-carrito" id="verCarrito"><i><lord-icon class="icon"src="https://cdn.lordicon.com/lqsduwhb.json" trigger="hover">
-    </lord-icon></i><span id="cantidadCarrito" class="cantidad-carrito"></span> </h2>
-      <div id="toggle">
-        <i class="indicador"></i>
-      </div>
-    </nav>
+        <div className="container">
+            <nav className="nav">
+                <div className="nav-brand">
+                    <a className="nav-link" href="#">Santa Pizza</a>
+                </div>
+                <ul className="nav-list">
+                    <li>
+                        <a className="nav-link" href="#">Inicio</a>
+                    </li>
+                    <li>
+                        <a className="nav-link" href="#">Productos</a>
+                    </li>
+                    <li>
+                        <a className="nav-link" href="#">
+                            <CartWidget/>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
 };
 
