99c8d151d5ba7928893f7eae0015bbed1a87b236
matisantacruzz <matias95ssc@gmail.com>
2023-04-12 10:38:20 -0300

    actualizacion codigo sobre suma de prods carrito; finalizacion compra con orden ID; tmb adición de producto; subida de localstorage-spinner-form-widget

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar';
+import Cart from './components/Cart/Cart'
+import Form from './components/Form/Form'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import CartProvider from './context/Cartcontext';
+
+
+function App() {
+  return (
+  <>
+    <CartProvider>  
+      <BrowserRouter>
+          <Navbar />
+            <Routes>
+              <Route path="/" element={<ItemListContainer greetings="Bienvenidos a Santa's Beer&Burger" />} />
+              <Route path="/categoria/:idCategoria" element={<ItemListContainer />} />
+              <Route path="/item/:iditem" element={ <ItemDetailContainer />} />
+              <Route path="/cart" element={ <Cart /> } />
+              <Route path="/Form" element={ <Form /> } />
+              <Route path="*" element={ <Navigate to="/" />}  />
+            </Routes>
+        </BrowserRouter>
+      </CartProvider>
+    </>
+  );
+}
+
+export default App;
